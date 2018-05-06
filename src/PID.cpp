bfa4a6a31e017d7c251ff57f95d483cdb122fd19
Ming Lin <flaaud159@gmail.com>
2018-05-06 14:15:47 +0900

    Update PID.cpp

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,26 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	Kp = Kp;
+	Ki = Ki;
+	Kd = Kd;
+	prev_cte = 0;
 }
 
 void PID::UpdateError(double cte) {
+
+	p_error = cte;
+
+	i_error = i_error + cte;
+
+	d_error = cte - prev_cte;
+
+	prev_cte = cte;
+	
 }
 
 double PID::TotalError() {
+	cte = Kp * cte + Kd * diff_cte / delta_t + Ki * sum_cte;
+	return cte;
 }
 
